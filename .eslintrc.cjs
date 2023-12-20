242fe04aed4bc194c62ee8dd19a5e8af4a1fbda6
Roberto Jasinski <roberto.jasinski@nearshoreportugal.pt>
2023-12-20 20:45:50 -0300

    build(app): setup project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    'cypress/globals': true,
+  },
+  plugins: ['vue', 'prettier-vue', 'cypress'],
+  globals: {
+    describe: 'readonly',
+    it: 'readonly',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier-vue/recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
+      }
+    ]
+  }
+}
